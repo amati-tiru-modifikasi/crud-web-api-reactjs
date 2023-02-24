d9fbcc38fc848ef2a71dfdf958eea655e2284ad0
mdestafadilah <desta.08b@gmail.com>
2023-02-24 23:53:31 +0700

    Update - install newstonjson - config cors

--- WebApplicationApi/WebApplicationApi/Program.cs
+++ WebApplicationApi/WebApplicationApi/Program.cs
@@ -1,4 +1,7 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
@@ -6,15 +9,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                        policy.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .SetIsOriginAllowed(origin => true)
+                              .AllowCredentials()
+                              .WithOrigins("https://localhost:44454","https://persahabatan.co.id");
+                      });
+});
+
+builder.Services.AddControllers().AddNewtonsoftJson(Options =>
+Options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(
+    options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
